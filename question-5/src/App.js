e423570909fba9b30734794301bd9580eabd54f0
Razi <syedru@mcmaster.ca>
2022-01-31 17:26:08 -0500

    Create react app for question 5

---
+++ question-5/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+ const [word, setWord] = useState();
+
+  return (
+    <div>
+      <p>Enter a word:</p>
+      <input />
+    </div>
+  );
+}
+
+export default App;
