298747014bec9570982d85c7ef3c33b785bf68e8
Razi <syedru@mcmaster.ca>
2022-01-31 12:15:48 -0500

    Add css to implement basic format

--- question-2/src/App.js
+++ question-2/src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
-import "./App.css";
+import classes from "./App.module.css";
 
 const App = () => {
   return (
-    <form>
+    <form className={classes.form}>
+      <label>Your full given name:</label>
+      <input type="text" />
+      <div>
+        <div className={classes.dob}>
+          <label>Date of Birth:</label>
+          <input type="text" />
+        </div>
+        <div className={classes.country}>
+          <label>Country of residence or citizenship:</label>
+          <input type="text" />
+        </div>
+      </div>
+      <label>What school do you plan to attend?</label>
+      <input type="text" />
       <label>
-        Your full given name:
-        <input type="text"></input>
+        Please take a moment to describe your intended area of study:
       </label>
-      <label>
-        Date of Birth:
-        <input type="text"></input>
-      </label>
-      <label>
-        Country of residence or citizenship:
-        <input type="text"></input>
-      </label>
-      <label>
-        What school do you plan to attend?
-        <input type="text"></input>
-      </label>
-      <label>
-      Please take a moment to describe your intended area of study:
-        <textarea>Hello</textarea>
-      </label>
+      <textarea>Hello</textarea>
     </form>
   );
 };
