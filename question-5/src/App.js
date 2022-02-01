7c81e01da4e8b676aced54a90410a98239693b1b
Razi <syedru@mcmaster.ca>
2022-01-31 19:47:43 -0500

    Implement functionality for app
    When Find is clicked, the rhymes for the entered word are fetched and
    then displayed

--- question-5/src/App.js
+++ question-5/src/App.js
@@ -1,12 +1,34 @@
 import { useState } from "react";
+import "./App.module.css";
 
 function App() {
- const [word, setWord] = useState();
+  const [word, setWord] = useState("");
+  const [rhymes, setRhymes] = useState([]);
 
+  const findRhymes = async (event) => {
+    event.preventDefault();
+    const result = await fetch(
+      `https://api.datamuse.com/words?rel_rhy=${word}`
+    );
+    const data = await result.json();
+    setRhymes(data.map((wData) => wData.word));
+  };
   return (
     <div>
-      <p>Enter a word:</p>
-      <input />
+      <p>Enter a word and click find to find words that rhyme.</p>
+      <form>
+        <input
+          type="text"
+          onChange={(event) => setWord(event.target.value)}
+          value={word}
+        />
+        <button onClick={findRhymes}>Find</button>
+        <ul>
+          {rhymes.map((rhyme) => (
+            <li key={rhyme}>{rhyme}</li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
